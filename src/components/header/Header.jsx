09fa5aea06f96b52d2079d2b3de8bf1c854ed43b
Ana Chiaramonte <anasenai.lopes@gmail.com>
2025-04-02 16:52:52 -0300

    fazendo as paginas do e modificando o header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,10 +3,10 @@ import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark ">
-      <div className="container">
-        <Link className="navbar-brand" to="/">
-      <img src="src/assets/Black White Minimalist Book Club Logo.png" alt="" />
+    <nav style={{ marginRight: "100px" }} className="navbar navbar-expand-lg navbar-dark ">
+      <div className="container ">
+        <Link   className="navbar-brand " to="/">
+      <img src="src/assets/Black White Minimalist Book Club Logo.png"   className="w-50 mt-n3  "alt="" />
         </Link>
         <button
           className="navbar-toggler"
@@ -18,19 +18,14 @@ const Header = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
+            <li className="nav-item ">
+              <Link className="nav-link mt-5 fs-4" to="/Livros#navbar">
+              <p style={{ color: "#533E39" }}>Livros</p>
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/sobre#navbar">
-               Livros
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contato#navbar">
-                Contato
+              <Link className="nav-link mt-5 m-5 fs-4 " to="/contato#navbar">
+              <p style={{ color: "#533E39" }}>  Progresso Leitura</p>
               </Link>
             </li>
           </ul>
