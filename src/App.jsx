0606bec4fa407417b344f58077ac0feffe34a789
Najul1a <158314850+Najul1a@users.noreply.github.com>
2025-05-14 12:28:05 +0000

    importanto o progresso de livros

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import ProgressaoDeLeitura from "./pages/ProgressaoDeLeitura";
 import "./App.css";
-
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import Header from "./components/header/Header";
 import Home from "./pages/Home";
-
 import Livros from "./pages/Livros";
 
 
@@ -24,6 +23,7 @@ function App() {
         </main>
 
       </BrowserRouter>
+      <ProgressaoDeLeitura/>
     </>
   );
 }
