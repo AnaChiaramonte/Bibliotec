70751538a16ed322096b56982b494de10c783c35
Ana Chiaramonte <anasenai.lopes@gmail.com>
2025-05-27 11:46:54 -0300

    Pagina Inicial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
 
   <h5 className='titulo-1 '>Controle de Progresso de Leitura</h5>
 
-  <svg xmlns="http://www.w3.org/2000/svg" className='img-grafico position-absolute o text-end d-flex ' width="100" height="100" fill="currentColor" class="bi bi-graph-up-arrow" viewBox="0 0 16 16">
+  <svg xmlns="http://www.w3.org/2000/svg" className='img-gra position-absolute o text-end d-flex ' width="100" height="100" fill="currentColor" class="bi bi-graph-up-arrow" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M0 0h1v15h15v1H0zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5"/>
 </svg>
 
