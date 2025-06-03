2021723e9c88c9fba3eff68afb54ae8f6d51cc4c
Ana Chiaramonte <anasenai.lopes@gmail.com>
2025-06-03 17:01:39 -0300

    arrumando o header para mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,17 +2,23 @@ import { Link } from "react-router";
 
 
 
-
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark  ">
-      <div className="container ">
-        <Link   className="logo m-5 " to="/">
-      <img src="src/assets/Black White Minimalist Book Club Logo.png"   className=" logo mt-0 m-0  "alt="" />
-        </Link>
+    <nav className="navbar navbar-expand-lg ">
+      <div className="container">
        
+        <Link className="navbar-brand" to="/">
+          <img
+            src="src/assets/Black White Minimalist Book Club Logo.png"
+            alt="Logo"
+            className="logo"
+            style={{ width: "60px" }}
+          />
+        </Link>
+
+        {/* Botão de menu mobile */}
         <button
-          className="navbar-toggler"
+          className="navbar-toggler "
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
@@ -22,31 +28,41 @@ const Header = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="navbar-nav collapse navbar-collapse" >
-          <ul className="navbar-nav">
-            <li className="nav-item ">
-              <Link className="nav-link mt-5 fs-4" to="/Livros">
-              <h5 >Livros</h5>
+
+        {/* Itens do menu */}
+        <div className="collapse navbar-collapse justify-content-center align-items-center" id="navbarNav">
+          <ul className="navbar-nav ms-auto align-items-center">
+            <li className="nav-item">
+              <Link className="nav-link fs-5" to="/Livros">
+                Livros
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link mt-5 m-5 fs-4 " to="/ProgressaoDeLeitura">
-              <h5>  Progresso Leitura</h5>
+              <Link className="nav-link fs-5" to="/ProgressaoDeLeitura">
+                Progresso de Leitura
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link fs-5" to="/Perfil">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  className="bi bi-person-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                </svg>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="btn btn-light ms-3" to="/Login">
+                Entrar
               </Link>
             </li>
           </ul>
         </div>
-        <div className="text-end position-relative">
-          <Link className="nav-link mt-3 m-3 fs-4 " to="/Perfil">
-        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
-  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-
-</svg>
-</Link>
-</div>
-<Link className="nav-link mt-2  fs-4 " to="/Login#navbar">
-<h5 className="ms-4">Entrar</h5>
-</Link>
       </div>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -92,7 +92,7 @@ const Home = () => {
       
 </div>
 <div >
-      <Footer   />
+    
     </div>
     </div>
   );
