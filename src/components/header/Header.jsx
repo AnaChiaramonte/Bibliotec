82b8bd21fdf3518e0c00d16d237b6ca4ca22db6f
Ana Geremias <anageremiias@gmail.com>
2025-06-04 11:57:11 -0300

    ajuste no header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import { useState } from "react";
 
 const Header = () => {
@@ -13,7 +13,7 @@ const Header = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark ">
+    <nav className="navbar navbar-expand-lg navbar-dark fs-5 fixed-top">
       <div className="container">
         <Link className="logo m-5" to="/" onClick={closeNavbar}>
           <img
@@ -36,38 +36,32 @@ const Header = () => {
             isOpen ? "show" : ""
           } justify-content-end`}
         >
-          <ul className="navbar-nav ">
-            <li className="nav-item">
-              <Link className="nav-link " to="/Livros" onClick={closeNavbar}>
-                Livros
-              </Link>
-            </li>
-            <li className="nav-item ">
-              <Link className="nav-link" to="/ProgressaoDeLeitura" onClick={closeNavbar}>
-                Progresso de Leitura
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link " to="/Perfil" onClick={closeNavbar}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  fill="currentColor"
-                  className="bi bi-person-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-                </svg>
-                
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white" to="/Login" onClick={closeNavbar}>
-                Entrar
-              </Link>
-            </li>
-          </ul>
+        <ul className="navbar-nav me-auto">
+  <li className="nav-item">
+    <Link className="nav-link" to="/Livros" onClick={closeNavbar}>
+      Livros
+    </Link>
+  </li>
+  <li className="nav-item">
+    <Link className="nav-link" to="/ProgressaoDeLeitura" onClick={closeNavbar}>
+      Progresso de Leitura
+    </Link>
+  </li>
+</ul>
+
+<div className="d-flex align-items-center ms-auto gap-2 mt-2">
+  <Link className="nav-link" to="/Perfil" onClick={closeNavbar}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="25" fill="#533E39" className="bi bi-person-fill" viewBox="0 0 16 16">
+      <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0-6 0 3 3 0 0 0 6 0z"/>
+    </svg>
+  </Link>
+
+  <Link className="nav-link text-white" to="/Login" onClick={closeNavbar}>
+    Entrar
+  </Link>
+</div>
+
+       
         </div>
       </div>
     </nav>
